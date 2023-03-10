7b16d6fb0b31f6ee371a5fa4b767fb2f8b83a141
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2023-03-10 19:46:01 -0300

    second solution for longest-common-prefix

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -25,3 +25,21 @@ var longestCommonPrefix = function(words) {
   }
   return prefix;
 };
+
+
+// Second solution (A little more efficient)
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+const longestCommonPrefix = function(words) {
+  if (words.length === 0) return '';
+  let prefix = words[0];
+  for (let i = 1; i < words.length; i++) {
+    while(words[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1)
+    }
+  }
+  return prefix;
+};
