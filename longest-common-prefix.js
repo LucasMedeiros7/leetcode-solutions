c8b0fcffaf6e9cb90c2bbb3ef2675839a4b51359
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2023-03-10 15:19:06 -0300

    14. longest common prefix

---
+++ longest-common-prefix.js
@@ -0,0 +1,27 @@
+// 14. Longest Common Prefix - LeetCode
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var longestCommonPrefix = function(words) {
+  if (words.length === 0) return '';
+  if (words.length === 1) return words[0];
+
+  const firstStr = words[0];
+  let prefix = '';
+  let letterPosition = 0;
+
+  while (letterPosition < firstStr.length) {
+    let repeat = 0;
+    for (let i = 1; i < words.length; i++) {
+      const isEqual = firstStr[letterPosition] === words[i][letterPosition];
+      if (isEqual) repeat++;
+    }
+    const isValidPrefix = repeat === (words.length - 1);
+    if (!isValidPrefix) return prefix;
+    prefix += firstStr[letterPosition];
+    letterPosition++;
+  }
+  return prefix;
+};
